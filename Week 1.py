837a73ff7b0b53bb79e6b4aa9779b66a91c1f5a2
Rasheedoh <mugirashid@gmail.com>
2025-04-16 22:35:13 +0300

    Python assignments

---
+++ Week 1.py
@@ -0,0 +1,5 @@
+num1=int(input("ENTER Number 1:"))
+num2=int(input("ENTER NUMBER 2:"))
+
+#THE OUTPUT SUM AND PRODUCT
+print("The sum of the two number is {0} \nThe product of the two numbers is{1}".format(num1+num2,num1*num2))
---
+++ week 2.py
@@ -0,0 +1,4 @@
+greeting="parrot"
+print(greeting)
+for i in range (0,10):
+    print(i)
